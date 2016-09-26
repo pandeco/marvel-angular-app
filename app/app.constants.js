fe7e35655df741c03c8797e6e1f2aeaaee332ecd
pasouza <pasouza@motorola.com>
2016-09-26 11:03:45 -0300

    new project. app.js added

---
+++ app/app.constants.js
@@ -0,0 +1,10 @@
+(function(){
+	'use strict';
+	/**
+	 * This template is used for creating app.constants.js, this file is used for creating the module m4.constants
+	 */
+	angular.module('marvel.constants',[])
+		.constant('API', 'http://gateway.marvel.com:80/v1/public/')
+		.constant('API_KEY', 'e51a3096420df03a123b8b3b312c5afc')
+
+})();
---
+++ app/app.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+
+    angular.module('marvel', ['marvel.constants', 'marvel.core', 'marvel.common', 'ui.bootstrap']);
+
+    angular.module('marvel.common', []);
+    angular.module('marvel.core', []);
+
+})();
