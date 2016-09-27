4aaa6c04a074527d65ee0fdd429ae5a68684ee48
pandeco <paulonh@gmail.com>
2016-09-27 05:58:49 -0300

    angularjs lib added

--- app/app.constants.js
+++ app/app.constants.js
@@ -1,10 +1,8 @@
 (function(){
 	'use strict';
-	/**
-	 * This template is used for creating app.constants.js, this file is used for creating the module m4.constants
-	 */
+
 	angular.module('marvel.constants',[])
-		.constant('API', 'http://gateway.marvel.com:80/v1/public/')
+		.constant('API_URL', 'http://gateway.marvel.com:80/v1/public/')
 		.constant('API_KEY', 'e51a3096420df03a123b8b3b312c5afc')
 
 })();
